1ab02b1e44334375e05f665a27b91247c22cf0eb
ParkIlKwon <qkrdlfrnjs95@naver.com>
2023-04-05 11:05:56 +0900

    add Adress

---
+++ Group3_Final-Project/src/main/java/com/boot/pro/domain/Address.java
@@ -0,0 +1,20 @@
+package com.boot.pro.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String address_name;
+    private String road_address_name;
+    protected Address(){}
+
+    public Address(String address_name, String road_address_name) {
+        this.address_name = address_name;
+        this.road_address_name = road_address_name;
+    }
+
+}
