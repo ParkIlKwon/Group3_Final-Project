e7f11bb6adcc024309b6be2b5bbb37272ef41401
ParkIlKwon <qkrdlfrnjs95@naver.com>
2023-04-05 12:15:44 +0900

    ad

--- Group3_Final-Project/src/main/java/com/jpa/intra/api/MemberApiController.java
+++ Group3_Final-Project/src/main/java/com/jpa/intra/api/MemberApiController.java
@@ -1,19 +1,30 @@
 package com.jpa.intra.api;
 
+import com.jpa.intra.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class MemberApiController {
 
-    @PostMapping(value = "/join")
 
+
+    @PostMapping(value = "/join")
+    public AddMember add(@RequestBody @Valid ){
+        Member member = new Member();
+        member.setId();
+
+    }
 
 
 
@@ -27,11 +38,19 @@ public class MemberApiController {
 
     @Data
     @AllArgsConstructor
-    static class MemberDto{
+    static class AddMember{
         private String name;
+
     }
 
+    @Data
+    static class CheckEffect{
+        @NotBlank(message = "아이디입력필수")
+        private String id;
 
+
+
+    }
 
 
 }
