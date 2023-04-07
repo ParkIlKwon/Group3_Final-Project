726893f6d3f7fe38eaef35d6eb85d3061fa98bc5
KIMBEOMJIN <kimbj0117@gmail.com>
2023-04-07 17:57:38 +0900

    jn

---
+++ Group3_Final-Project/src/main/java/com/jpa/intra/controller/PageController.java
@@ -0,0 +1,22 @@
+package com.jpa.intra.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/pages")
+public class PageController {
+
+    @GetMapping("/dashboard")
+    public String pagesDashboard(){
+        return "/pages/dashboard";
+    }
+
+    @GetMapping("/project")
+    public String pagesProject(){
+        return "/pages/project";
+    }
+}
