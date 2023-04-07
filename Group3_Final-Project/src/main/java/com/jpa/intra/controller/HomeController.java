ab1db83251b16016e4f03e4825f9740a73e877fd
YangjaeHyeok <102141329+jjuhye@users.noreply.github.com>
2023-04-07 14:46:34 +0900

    .

--- Group3_Final-Project/src/main/java/com/jpa/intra/controller/HomeController.java
+++ Group3_Final-Project/src/main/java/com/jpa/intra/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     public String home(Model model) {
         model.addAttribute("banner","banner1");
         model.addAttribute("side","sidebar9");
-        return "/pages/dashboard";
+        return "/home";
     }
 
 }
