6d5dc5224be6d7664cfe67b8f0d45bc102cf231f
ParkIlKwon <qkrdlfrnjs95@naver.com>
2023-04-10 14:31:45 +0900

    fadad

--- Group3_Final-Project/src/main/java/com/jpa/intra/domain/board/BoardApproval.java
+++ Group3_Final-Project/src/main/java/com/jpa/intra/domain/board/BoardApproval.java
@@ -20,21 +20,21 @@ public class BoardApproval extends BoardCommon {
     //결재시작일 (시작일과)
     //결재마감일 (마감일을 통해 언제까지 받아주는 사람이 이걸 승인 반려해줄지 기간관련정보)
     //결재상세정보 (결재 타입에 따라 다른 데이터를 넣을 수 있도록 JSON 타입으로 생성)
-    @ManyToOne
-    @JoinColumn(name="mem_num")
-    @Convert(converter= MemberConverter.class)
-    private Member requestorMemNum;
-
-    @ManyToOne
-    @JoinColumn(name="mem_num")
-    @Convert(converter= MemberConverter.class)
-    private Member approverMemNum;
-
-    private String approvalType;
-    private String approvalStatus;
-
-    private String startDate;
-    private String dueDate;
-
-    private ObjectMapper approvalInfo;
+//    @ManyToOne
+//    @JoinColumn(name="mem_num")
+//    @Convert(converter= MemberConverter.class)
+//    private Member requestorMemNum;
+//
+//    @ManyToOne
+//    @JoinColumn(name="mem_num")
+//    @Convert(converter= MemberConverter.class)
+//    private Member approverMemNum;
+//
+//    private String approvalType;
+//    private String approvalStatus;
+//
+//    private String startDate;
+//    private String dueDate;
+//
+//    private ObjectMapper approvalInfo;
 }
