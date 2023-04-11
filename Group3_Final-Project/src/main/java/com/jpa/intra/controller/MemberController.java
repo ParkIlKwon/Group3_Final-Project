046fdeb29d1596856f4fb6efd1d69dd00b7a5a56
KIMBEOMJIN <kimbj0117@gmail.com>
2023-04-11 18:24:20 +0900

    .
    .

--- Group3_Final-Project/src/main/java/com/jpa/intra/controller/MemberController.java
+++ Group3_Final-Project/src/main/java/com/jpa/intra/controller/MemberController.java
@@ -2,8 +2,8 @@ package com.jpa.intra.controller;
 
 import com.jpa.intra.domain.Address;
 import com.jpa.intra.domain.Member;
-import com.jpa.intra.query.LoginDTO;
 import com.jpa.intra.query.MemberDTO;
+import com.jpa.intra.repository.Member_Repository;
 import com.jpa.intra.service.MailService;
 import com.jpa.intra.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller //model 을 지정 , view를 반환 시켜주는 컨트롤러 역활
@@ -24,6 +25,8 @@ import javax.validation.Valid;
 public class MemberController {
 
     private final MemberService service;
+    private final Member_Repository member_repository;
+
     @GetMapping()
     public String addForm(Model model){
 
@@ -117,5 +120,10 @@ public class MemberController {
         return code;
     }
 
-
+    @GetMapping("/main")
+    public String mailMain(Model model){
+        List<Member> memberList = member_repository.getAllMemberList();
+        model.addAttribute("memberList",memberList);
+        return "members/main";
+    }
 }
