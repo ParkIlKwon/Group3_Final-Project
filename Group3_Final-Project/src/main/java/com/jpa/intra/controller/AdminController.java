5aded57b54ea877e44b91b3c6f41a429430fe062
jjuhye <102141329+jjuhye@users.noreply.github.com>
2023-04-12 14:42:00 +0900

    230412_admin

--- Group3_Final-Project/src/main/java/com/jpa/intra/controller/AdminController.java
+++ Group3_Final-Project/src/main/java/com/jpa/intra/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.jpa.intra.controller;
 
+import com.jpa.intra.domain.Member;
 import com.jpa.intra.query.MemberDTO;
+import com.jpa.intra.repository.Member_Repository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller //model 을 지정 , view를 반환 시켜주는 컨트롤러 역활
 @RequiredArgsConstructor //생성자 주입
 @RequestMapping("/admin") //공통된 폴더명 하나로 묶어서 사용
 public class AdminController {
+
+    private final Member_Repository member_repository;
 
     @GetMapping()
     public String adminMain(Model model){
@@ -22,11 +27,18 @@ public class AdminController {
         return "admin/main";
     }
 
-    @GetMapping("/logout")
-    public String Logout(HttpServletRequest request){
-        HttpSession session = request.getSession();
-        session.invalidate();
-        return "pages/loginForm";
+    @GetMapping("/admin_member")
+    public String admin_member(Model model){
+        List<Member> memberList = member_repository.getAllMemberList();
+        model.addAttribute("memberList",memberList);
+        model.addAttribute("page", "관리자");
+        return "admin/admin_member";
+    }
+    @GetMapping("/join")
+    public String memberJoin(Model model){
+        model.addAttribute("page", "관리자");
+//        return "pages/joinFormAlpa";
+        return "admin/joinForm";
     }
 
 }
