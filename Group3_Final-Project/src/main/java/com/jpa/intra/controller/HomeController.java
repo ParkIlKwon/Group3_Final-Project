7a17e45a36f06766c540f5b61d3225b6cf702556
ParkIlKwon <qkrdlfrnjs95@naver.com>
2023-04-22 13:34:03 +0900

    add annotation

--- Group3_Final-Project/src/main/java/com/jpa/intra/controller/HomeController.java
+++ Group3_Final-Project/src/main/java/com/jpa/intra/controller/HomeController.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@Slf4j
+@Slf4j //Simple Logging Facade for Java)
+//오류 메시지 확인용도
 @RequiredArgsConstructor
 public class HomeController {
 
